12d009ea2a38ba498729f17cc484178510ced468
Amy Westcott <amy.westcott@yahoo.com>
2020-07-06 16:54:09 -0600

    created header for the calender with correct dates

--- client/src/containers/Calendar.js
+++ client/src/containers/Calendar.js
@@ -6,42 +6,66 @@ export class Calendar extends Component {
   constructor() {
     super()
     this.state = {
-      dateObject: moment()
+      dateContext: moment(),
+      today: moment(),
+      selectedDay: null
     }
-  }
-
-  createDate = () => {
-    let dateObject = this.state.dateObject;
-    let startDate = moment(dateObject).subtract(3, 'days').format("MMM Do");  ;
-  
-    return startDate ;
-
   }
 
   createWeek = () => {
-    let week = [];
-    let day = this.createDate()
-    for (let d = 0; d <= 6; d++) {
-      week.push(
-        <td key={d} className="calendar-day">
-          {day} 
-        </td>
-      );
-     
-    }
-    return (
-      week
-    )
-}
+    let dateObject = this.state.dateObject;
+    let week = []
+    let startDate = moment(dateObject).subtract(3, 'days').format("MMM Do");  
+    let secondDate = moment(dateObject).subtract(2, 'days').format("MMM Do");  
+    let thirdDate = moment(dateObject).subtract(1, 'days').format("MMM Do"); 
+    let fourthDate = moment(dateObject).format("MMM Do"); 
+    let fifthDate = moment(dateObject).add(1, 'days').format("MMM Do");
+    let sixthDate = moment(dateObject).add(2, 'days').format("MMM Do");
+    let seventhDate = moment(dateObject).add(3, 'days').format("MMM Do");
+    week.push(startDate, secondDate, thirdDate, fourthDate, fifthDate, sixthDate, seventhDate)
+
+    return week
+  }
+
+  // startDate = () => {
+  //   let dateObject = this.state.dateObject;
+  //   let startDate = moment(dateObject).subtract(3, 'days').format("MMM Do");  
+  
+  //   return startDate ;
+
+  // }
+
+  // nextDay = () => {
+  //   let dateObject = this.state.dateContext;
+  //   let nextDay = moment(dateObject).add(1, 'days').format("MMM Do");
+
+  //   return nextDay ; 
+  // }
+
+
+  // createWeek = () => {
+  //   let week = [];
+  //   let day = this.startDate()
+  //   for (let d = 0; d <= 6; d++) {
+  //     week.push(
+  //       <td key={d} className="calendar-day">
+  //         {day} 
+  //       </td>
+  //     );
+  //     day = moment(day).add(1, 'days').format("MMM Do");
+  //     debugger;
+  // }
+  //   return (
+  //     week
+  //   )
+  // }
     
       renderHeader() {
-        let date = this.createDate()
         let week = this.createWeek()
 
 
         return (
-          <div className="card">
-              { date }  
+          <div className="card">  
               {week}
           </div>
           
